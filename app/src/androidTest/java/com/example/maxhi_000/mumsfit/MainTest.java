729eb4cc400d8cb294c41671bc045ebee5264f22
MH0896 <max.hirte@web.de>
2017-05-04 14:32:48 +0200

    Updated Gradle with Lint-Option and Test-Suits^^

--- app/src/androidTest/java/com/example/maxhi_000/mumsfit/MainTest.java
+++ app/src/androidTest/java/com/example/maxhi_000/mumsfit/MainTest.java
@@ -53,7 +53,6 @@ public class MainTest {
 
     @Test
     public void useAppContext() {
-
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
@@ -134,56 +133,4 @@ public class MainTest {
         Thread.sleep(1000);
         assertFalse(ViewPlan.getItemAtPosition(1) == "Unit-Test");
     }
-
-/*
-    @Test
-    public void seeMovieDetails() throws MovieDbException {
-        Instrumentation.ActivityMonitor monitor = InstrumentationRegistry.getInstrumentation().addMonitor(MovieDetailsActivity.class.getName(), null, false);
-
-        final AddMovieActivity addMovieActivity = mActivityRule.getActivity();
-
-        final ListView queryList = (ListView) addMovieActivity.findViewById(R.id.queryListView);
-
-        addMovieActivity.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-
-                queryList.performItemClick(queryList, 0, 0);
-
-            }
-        });
-
-        MovieInfo selectedMovie = (MovieInfo) queryList.getAdapter().getItem(0);
-
-        View row = queryList.getChildAt(0);
-
-        row.findViewById(R.id.movieImage);
-
-        String ratingScore;
-
-        if (selectedMovie.getPopularity() > 10) {
-            ratingScore = "10";
-        } else {
-            ratingScore = Float.toString(selectedMovie.getPopularity()).substring(0, 3);
-        }
-
-        String expectedDesc = selectedMovie.getOverview();
-        String expectedGenre = HelperFunctions.getInstance().getGenreMap("de").get(selectedMovie.getGenreIds().get(0)) + ", " + selectedMovie.getReleaseDate().substring(0, 4);
-        String expectedTitle = selectedMovie.getTitle();
-        String expectedRating = ratingScore + "/10";
-
-        MovieDetailsActivity movieDetails = (MovieDetailsActivity) monitor.waitForActivity();
-
-        assertNotNull(movieDetails);
-
-        TextView desc = (TextView) movieDetails.findViewById(R.id.movie_desc);
-        TextView title = (TextView) movieDetails.findViewById(R.id.title);
-        TextView rating = (TextView) movieDetails.findViewById(R.id.rating);
-        TextView genre = (TextView) movieDetails.findViewById(R.id.genre);
-
-        assertEquals(desc.getText(),expectedDesc);
-        assertEquals(title.getText(), expectedTitle);
-        assertEquals(rating.getText(), expectedRating);
-        assertEquals(genre.getText(), expectedGenre);
-    }*/
 }
