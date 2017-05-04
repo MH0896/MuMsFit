d38df47a021c8888b37bc2ca14df6b9c7bd8485c
MH0896 <max.hirte@web.de>
2017-05-04 14:55:13 +0200

    mainActivityTest

---
+++ app/src/test/java/com/example/maxhi_000/mumsfit/MainActivityTest.java
@@ -0,0 +1,61 @@
+package com.example.maxhi_000.mumsfit;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by maxhi_000 on 04.05.2017.
+ */
+public class MainActivityTest {
+    @Test
+    public void onCreate() throws Exception {
+        assertEquals(1,1);
+    }
+
+    @Test
+    public void checkEingabe() throws Exception {
+        assertEquals(1,1);
+    }
+
+    @Test
+    public void onCreateOptionsMenu() throws Exception {
+        assertEquals(1,1);
+    }
+
+    @Test
+    public void onOptionsItemSelected() throws Exception {
+        assertEquals(1,1);
+    }
+
+    @Test
+    public void onBackPressed() throws Exception {
+        assertEquals(1,1);
+    }
+
+    @Test
+    public void deleteClick() throws Exception {
+        assertEquals(1,1);
+    }
+
+    @Test
+    public void detailsClick() throws Exception {
+        assertEquals(1,1);
+    }
+
+    @Test
+    public void shareClick() throws Exception {
+        assertEquals(1,1);
+    }
+
+    @Test
+    public void editClick() throws Exception {
+        assertEquals(1,1);
+    }
+
+    @Test
+    public void analyzeClick() throws Exception {
+        assertEquals(1,1);
+    }
+
+}
