afdb4e3bf8359502fc714e9d89992a7148e12905
MH0896 <max.hirte@web.de>
2017-05-05 11:03:44 +0200

    UnitTests

--- app/src/test/java/com/example/maxhi_000/mumsfit/PlanTest.java
+++ app/src/test/java/com/example/maxhi_000/mumsfit/PlanTest.java
@@ -2,6 +2,9 @@ package com.example.maxhi_000.mumsfit;
 
 import org.junit.Test;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import static org.junit.Assert.*;
 
 /**
@@ -10,12 +13,15 @@ import static org.junit.Assert.*;
 public class PlanTest {
     @Test
     public void getPlan_id() throws Exception {
-
+        Plan testPlan = new Plan("toTest");
+        assertEquals(0, testPlan.getPlan_id());
     }
 
     @Test
     public void setPlan_id() throws Exception {
-
+        Plan testPlan = new Plan("toTest");
+        testPlan.setPlan_id(87);
+        assertEquals(87, testPlan.getPlan_id());
     }
 
     @Test
@@ -33,22 +39,32 @@ public class PlanTest {
 
     @Test
     public void getDate_create() throws Exception {
-
+        String date = new SimpleDateFormat("dd-MM-yyyy").format(new Date());
+        Plan testPlan = new Plan("toTest", date, "-");
+        assertEquals(date, testPlan.getDate_create());
     }
 
     @Test
     public void setDate_create() throws Exception {
-
+        String date = new SimpleDateFormat("dd-MM-yyyy").format(new Date());
+        Plan testPlan = new Plan("toTest", date, "-");
+        testPlan.setDate_create("newDate");
+        assertEquals("newDate", testPlan.getDate_create());
     }
 
     @Test
     public void getDate_last() throws Exception {
-
+        String date = new SimpleDateFormat("dd-MM-yyyy").format(new Date());
+        Plan testPlan = new Plan("toTest", "today", date);
+        assertEquals(date, testPlan.getDate_last());
     }
 
     @Test
     public void setDate_last() throws Exception {
-
+        String date = new SimpleDateFormat("dd-MM-yyyy").format(new Date());
+        Plan testPlan = new Plan("toTest", "today", date);
+        testPlan.setDate_last("newDate");
+        assertEquals("newDate", testPlan.getDate_last());
     }
 
 }
