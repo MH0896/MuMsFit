12cd146a7774a0c1ffc58fd72f587667a79418ff
MW210696 <Moritz.W@cker-online.de>
2017-05-17 13:21:52 +0200

    optimized imports

--- app/src/main/java/com/example/maxhi_000/mumsfit/MainActivity.java
+++ app/src/main/java/com/example/maxhi_000/mumsfit/MainActivity.java
@@ -14,7 +14,6 @@ import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.app.AppCompatDelegate;
 import android.text.InputType;
 import android.util.DisplayMetrics;
 import android.view.ActionMode;
